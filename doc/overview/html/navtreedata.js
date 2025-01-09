dbbff4e8e84007a2205784fa00f2304073d9c85e
akingse <48311823+akingse@users.noreply.github.com>
2025-01-09 23:10:09 +0800

    添加doc

---
+++ doc/overview/html/navtreedata.js
@@ -0,0 +1,93 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "Open CASCADE Technology", "index.html", [
+    [ "Introduction", "index.html", [
+      [ "Overview", "index.html#intro_overview", [
+        [ "Foundation Classes", "index.html#intro_overview_fclasses", null ],
+        [ "Modeling Data", "index.html#intro_overview_moddata", null ],
+        [ "Modeling Algorithms", "index.html#intro_overview_modalgo", null ],
+        [ "Mesh", "index.html#intro_overview_mesh", null ],
+        [ "Visualization", "index.html#intro_overview_visu", null ],
+        [ "Data Exchange", "index.html#intro_overview_de", null ],
+        [ "Shape Healing", "index.html#intro_overview_heal", null ],
+        [ "Application Framework", "index.html#intro_overview_ocaf", null ],
+        [ "Draw Test Harness", "index.html#intro_overview_draw", null ]
+      ] ],
+      [ "Requirements", "index.html#intro_req", [
+        [ "C++ Compiler / IDE", "index.html#intro_req_cpp", null ],
+        [ "Third-party libraries and tools", "index.html#intro_req_libs", null ],
+        [ "Hardware", "index.html#intro_req_hw", null ]
+      ] ],
+      [ "Download and Installation", "index.html#intro_install", [
+        [ "Windows", "index.html#intro_install_windows", null ],
+        [ "Linux", "index.html#intro_install_linux", null ],
+        [ "macOS", "index.html#intro_install_mac", null ]
+      ] ],
+      [ "Environment Variables", "index.html#intro_env", [
+        [ "On Windows", "index.html#autotoc_md0", null ],
+        [ "On Unix", "index.html#autotoc_md1", null ],
+        [ "Description of system variables:", "index.html#autotoc_md2", null ]
+      ] ],
+      [ "License", "index.html#intro_license", null ],
+      [ "Acknowledgments", "index.html#intro_acknowledge", null ]
+    ] ],
+    [ "Tutorials and Samples", "samples.html", "samples" ],
+    [ "Build, Debug and Upgrade", "build_upgrade.html", "build_upgrade" ],
+    [ "User Guides", "user_guides.html", "user_guides" ],
+    [ "Specifications", "specification.html", "specification" ],
+    [ "Contribution", "contribution.html", "contribution" ],
+    [ "License", "occt_public_license.html", [
+      [ "GNU LESSER GENERAL PUBLIC LICENSE", "occt_public_license.html#license_lgpl_21", [
+        [ "Preamble", "occt_public_license.html#autotoc_md407", null ],
+        [ "TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION", "occt_public_license.html#autotoc_md408", null ],
+        [ "END OF TERMS AND CONDITIONS", "occt_public_license.html#autotoc_md409", null ],
+        [ "How to Apply These Terms to Your New Libraries", "occt_public_license.html#autotoc_md410", null ]
+      ] ],
+      [ "OPEN CASCADE EXCEPTION", "occt_public_license.html#occt_lgpl_exception", [
+        [ "Open CASCADE Exception (version 1.0) to GNU LGPL version 2.1.", "occt_public_license.html#autotoc_md411", null ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"build_upgrade.html",
+"occt__upgrade.html#upgrade_occt700_correction_of_texture",
+"occt_contribution__tests.html#testmanual_5_1",
+"occt_user_guides__inspector.html#occt_inspector_2_3_5",
+"occt_user_guides__modeling_data.html#occt_modat_2",
+"occt_user_guides__shape_healing.html#occt_shg_5_",
+"occt_user_guides__test_harness.html#occt_draw_4_3_2",
+"occt_user_guides__test_harness.html#occt_draw_7_1_4",
+"occt_user_guides__visualization.html#autotoc_md192",
+"specification__boolean_operations.html#autotoc_md305",
+"specification__brep_format.html#specification__brep_format_5_3_1"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
